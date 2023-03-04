a8f85461a4e29d4d8a9aff277a21217c00a1d687
iamgauhar <iamgauhar@yahoo.com>
2023-03-04 12:07:18 +0530

    Library

---
+++ LibarayLld.js
@@ -0,0 +1,45 @@
+class Books {
+  constructor(){
+  	this.name = name, 
+    this.author = author, 
+    this.ISBN = ISBN,
+    this.isAvailable = status
+  }
+}
+
+class Library {
+	constructor(){
+  	this.books = []
+  }
+  
+  addNewBooks(){
+  	if(!this.searchForBooks){
+        
+    }
+    //add new books here.
+  }
+  
+  searchForBooks(){
+  	//searchForBooks
+  }
+  
+  borrowBooks(){
+  	if(checkForAvailability) 
+    	// makeThatBookStatus = false
+    	return "LET THEM BORROW"
+    
+  	//borrow Books here
+  }
+  
+  returnBooks(){
+  	if(this.searchForBooks && !this.checkForAvailabilty){
+
+    }
+  		//return Books here
+  }
+  
+  checkForAvailabilty(){
+  	return true ? bookAvailable : false
+  }
+  
+}
